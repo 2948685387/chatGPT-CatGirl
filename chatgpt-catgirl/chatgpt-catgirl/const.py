3eb08c55042cf60ad6c85e87875f0932ecd16c87
2948685387 <82927102+2948685387@users.noreply.github.com>
2023-03-11 10:35:00 +0800

    Add files via upload

---
+++ chatgpt-catgirl/chatgpt-catgirl/const.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+常量
+"""
+
+APP_ID = '31105443'
+
+API_KEY = 'wHpLMGFhMBKsvQQm1sIq3Em5'
+
+SECRET_KEY = 'jWkdACTpQsaNXejVp4pIMWrvu76Gs2fk'
+
+# 语言模型 ， 可以修改为其它语言模型测试，如远场普通话19362
+DEV_PID = 15372
+
+# 可以改为wss://
+URI = "ws://vop.baidu.com/realtime_asr"
+
+
+openai_api_key = "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"
