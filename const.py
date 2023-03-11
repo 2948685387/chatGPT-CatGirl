b8ab758e75e70ca3d33a630799d79def48580bc3
2948685387 <82927102+2948685387@users.noreply.github.com>
2023-03-11 10:45:16 +0800

    Create const.py

---
+++ const.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+常量
+"""
+
+APP_ID = '百度云'
+
+API_KEY = '百度云'
+
+SECRET_KEY = '百度云'
+
+# 语言模型 ， 可以修改为其它语言模型测试，如远场普通话19362
+DEV_PID = 15372
+
+# 可以改为wss://
+URI = "ws://vop.baidu.com/realtime_asr"
+
+
+openai_api_key = "你的OpenAI API密钥"
